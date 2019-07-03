0627e9e4c235b07ff4250a30ef699775697ec764
WittBulter <lambdas.dev@gmail.com>
2019-07-03 09:43:19 +0800

    chore: update username

--- src/components/slogan.vue
+++ src/components/slogan.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 div
   h3.title Easy Jsdelivr
-  p.zi-comment You can quickly find the target link from this page. #[zi-link(href='https://github.com/WittBulter/easy-jsdelivr' black more) Github]
+  p.zi-comment You can quickly find the target link from this page. #[zi-link(href='https://github.com/unix/easy-jsdelivr' black more) Github]
 </template>
 
 <style scoped lang="stylus">
